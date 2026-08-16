058524348b17ebb2625a8c3d1d41230714784792
Ayush Panda <pandaayush25305@gmail.com>
2026-08-16 23:40:14 +0530

    fix google oauth proxy settings

--- server/src/config/passport.js
+++ server/src/config/passport.js
@@ -16,6 +16,7 @@ export function initializePassport() {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: '/api/auth/google/callback',
+        proxy: true,
       },
       async (_accessToken, _refreshToken, profile, done) => {
         try {
